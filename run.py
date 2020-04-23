9b80bed2607fde0a90ff81ad321fc0f767d8617b
Yagiz <ygz_bzc@hotmail.com>
2020-04-23 16:58:39 +0300

    Deneme endpoint i eklendi.

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
 def home():
     return "<h1>It's working!</h1>"
+
+
+@app.route("/get", methods=["GET"])
+def get():
+    return {'Deneme': 'Başarılı'}
 
 
 if __name__ == "__main__":
