7041202b1fa4e158215ce551f4af82e07627e995
Yagiz <ygz_bzc@hotmail.com>
2020-04-23 20:50:43 +0300

    Yari çalişir halde.

--- run.py
+++ run.py
@@ -1,19 +1,25 @@
 from flask import Flask
+from flask_restplus import Api, Resource, fields
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+api = Api(app, version='1.0', title='WebRTC API',
+          description='WebRTC API', doc='/docs')
+
+ns = api.namespace('webrtc', description='WebRTC operations')
 
 
-@app.route("/")
+@ns.route("/get")
+class Get(Resource):
+    def get(self):
+        return {'Deneme': 'Başarılı'}
+
+
+@app.route("/", methods=["GET"])
 def home():
     return "<h1>It's working!</h1>"
 
 
-@app.route("/get", methods=["GET"])
-def get():
-    return {'Deneme': 'Başarılı'}
-
-
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
